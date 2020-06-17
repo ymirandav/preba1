7d5ce23a11e4336e9ce2f455520282907cf01c82
André Cruces <66932538+Shifu661@users.noreply.github.com>
2020-06-16 22:20:40 -0500

    Cambio 1
    Extra

---
+++ shifu1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hola yoshiro"<<endl; //modificacion
+    cout<<"eje y"<<endl;
+    cout<<"hola  mundo"<<endl;//hola
+    return 0;
+}
