6f8094b2afb757b00cf519489bef160ee65b86a2
Elvis <ecruceg@ulasalle.edu.pe>
2020-06-16 22:31:42 -0500

    Modificando1.0

---
+++ wos.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"que tal"<<endl; //modificacion
+    cout<<"lp de mierda"<<endl;
+    cout<<"profesora de ......."<<endl;//hola
+    return 0;
+}
